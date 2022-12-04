b93e56509c1e3cc2394aa66a7c3da92c1dc65176
Sertaç Gültekin <sertacgltkn@gmail.com>
2022-12-04 11:35:34 +0300

    item price edited

--- src/components/product-list/index.js
+++ src/components/product-list/index.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from "react";
+import { formatPrice } from "../../utils";
+import Button from "../button";
 import "./style.css";
 
 export default function ProductList({ products = [] }) {
-  const [items, setItems] = useState([]) 
+  const [items, setItems] = useState([]);
   useEffect(() => {
     handleProducts();
   }, [products]);
@@ -26,14 +28,22 @@ export default function ProductList({ products = [] }) {
 
   const renderCard = (item) => {
     return (
-      <div className="col-3">
+      <div className="col-3" key={item.id}>
         <div className="card" style={{ width: "18rem" }}>
           <img src={item.image_url} className="card-img-top" alt={item.name} />
           <div className="card-body">
             <h5 className="card-title">{item.name}</h5>
-            <a href="#" className="btn btn-primary">
-              Sepete Ekle
-            </a>
+            <div className="custom-card-footer">
+              <Button className="btn btn-primary">
+                Sepete Ekle
+              </Button>
+              <span>{formatPrice(item.price)}</span>{" "}
+              {/* toFixed(2) js methodu : fiyatın .'dan sonraki küsüratını 2 haneli yapar. */}
+              
+                 {/* formatPrice bizim yazdığımız bir method ve utils klasörü içinde */} 
+             
+              {/* TL işareti için alt gr+ T = ₺ */}
+            </div>
           </div>
         </div>
       </div>
@@ -48,7 +58,7 @@ export default function ProductList({ products = [] }) {
             <div className="col-10">
               <div className="category-title">{item.name}</div>
             </div>
-            <div className="col-2">
+            <div className="col-2"> 
               <div>Tümünü Gör</div>
             </div>
             <div className="row product-row">
---
+++ src/utils/index.js
@@ -0,0 +1,9 @@
+/**
+ * 
+ * @param {number} price 
+ * @returns 
+ */
+
+export function formatPrice(price) {
+    return `${price.toFixed(2)}  ₺`
+}
